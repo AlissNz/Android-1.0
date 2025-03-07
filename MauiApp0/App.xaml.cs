9439ae7725f14131f23fb5c64c5f3e744aee6044
Alisson Luiz <77544592+AlissNz@users.noreply.github.com>
2025-03-07 11:07:24 -0300

    Update App.xaml.cs

--- MauiApp0/App.xaml.cs
+++ MauiApp0/App.xaml.cs
@@ -1,12 +1,35 @@
-﻿namespace MauiApp0
+using MauiApp0.Helpers;
+
+namespace MauiApp0
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new views.ListaProduto());
         }
     }
 }
