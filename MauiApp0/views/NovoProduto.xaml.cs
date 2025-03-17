12d45db7f86040655b1e63247e6a424418cc1837
Alisson Luiz <77544592+AlissNz@users.noreply.github.com>
2025-03-16 22:40:30 -0300

    Update NovoProduto.xaml.cs

--- MauiApp0/views/NovoProduto.xaml.cs
+++ MauiApp0/views/NovoProduto.xaml.cs
@@ -16,7 +16,7 @@ public partial class NovoProduto : ContentPage
             Produto p = new Produto
             {
                 Descricao = txt_descricao.Text,
-                Quatidade = Convert.ToDouble(txt_quantidade.Text),
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
                 Preco = Convert.ToDouble(txt_preco.Text)
             };
 
